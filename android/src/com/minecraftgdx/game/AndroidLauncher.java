aa0ac6229dbf1971ecb7160d857b8f8799b08cf5
AlexG <alexmichelito2016@gmail.com>
2024-04-21 16:57:12 -0500

    Finally it works omg

---
+++ android/src/com/minecraftgdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.minecraftgdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.minecraftgdx.game.MyMinecraftgdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyMinecraftgdxGame(), config);
+	}
+}
